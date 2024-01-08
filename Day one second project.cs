cacc7d73eb50b0b0e9c6d4eb670c5fc5d4480f0e
xX_ShakeNotDev_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-01-08 19:13:58 -0300

    Create Day one second project.cs

---
+++ Day one second project.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace content
+{
+    class DayOneProject2
+    {
+        static void Main(string[] args)
+        {
+            var project = new DayOneProject2();
+            Console.WriteLine("Insert the first value: ");
+            float x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert the second value: ");
+            float y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert an option: ");
+            String option = Console.ReadLine();
+
+            if (option == "sum" || option == "Sum")
+            {
+                Console.WriteLine("The sum is: "+sum(x, y));
+            }
+            else if (option == "subtration" || option == "Subtration")
+            {
+                Console.WriteLine("The subtration is: "+subtration(x,y));
+            }
+            else if (option == "multiplication" || option == "Multiplication")
+            {
+                Console.WriteLine("The multiplication is: "+multiplication(x,y));
+            }
+            else if (option == "division" || option == "Division")
+            {
+                Console.WriteLine("The division is: " + division(x, y));
+            }
+        }
+
+        static float division(float x, float y)
+        {
+            return x / y;
+        }
+        static float multiplication(float x, float y)
+        {
+            return x * y;
+        }
+        static float subtration(float x, float y)
+        {
+            return x - y;
+        }
+        static float sum(float n1 , float n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
