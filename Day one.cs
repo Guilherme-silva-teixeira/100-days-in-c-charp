126313aba59543c9e12cf8fd1bd7e36709b80588
xX_ShakeNotDev_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-01-08 17:42:07 -0300

    Update Day one.cs

--- Day one.cs
+++ Day one.cs
@@ -7,6 +7,8 @@ namespace Day_one
         static void Main(String[] args)
         {
             First();
+            Second();
+            Third();
         }
 
         static void First()
@@ -17,8 +19,17 @@ namespace Day_one
         //variables
         static int Second()
         {
-
+            int n1 = 2;
+            int n2 = 3;
+            int res = n1 + n2;
+            Console.WriteLine(res);
             return 1;
+        }
+
+        static void Third()
+        {
+            String name = Console.ReadLine();
+            Console.WriteLine(name);
         }
     }
 }
