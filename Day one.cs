52b61e9078ab7fb7e5fb6b655df55ec8b996af1f
xX_ShakeNotDev_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-01-08 16:33:27 -0300

    Update and rename day one.cSharp to Day one.cs

---
+++ Day one.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Day_one
+{
+    class dayOne
+    {
+        static void Main(String[] args)
+        {
+            First();
+        }
+
+        static void First()
+        {
+            Console.WriteLine("Hello, World");
+        }
+
+        //variables
+        static int Second()
+        {
+
+            return 1;
+        }
+    }
+}
