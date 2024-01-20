98b3bbfaeb3e99c02beb798a657cb43d453c6329
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-01-20 17:09:10 -0300

    Update and rename delete.cs to Delete.cs

--- Day 7/Pages/Movies/Delete.cs
+++ Day 7/Pages/Movies/Delete.cs
@@ -13,25 +13,25 @@
     <hr />
     <dl classeeeeeeeeee="linha">
         <dt classeeeeeeeeee="col-sm-2">
-            @Html. .DisplayNameFor (modelo => modelo.Movie.Title)
+            @Html .DisplayNameFor (modelo => modelo.Movie.Title).
         </dt>
         <dd classeeeeeeeeee="col-sm-10">
-            @Html. .DisplayFor (modelo => modelo.Movie.Title)
+            @Html .DisplayFor (modelo => modelo.Movie.Title).
         </dd>
         <dt classeeeeeeeeee="col-sm-2">
-            @Html. .DisplayNameFor (modelo => model.Movie.ReleaseDate)
+            @Html .DisplayNameFor (modelo => model.Movie.ReleaseDate).
         </dt>
         <dd classeeeeeeeeee="col-sm-10">
-            @Html. .DisplayFor (modelo => modelo.Movie.ReleaseDate)
+            @Html .DisplayFor (modelo => modelo.Movie.ReleaseDate).
         </dd>
         <dt classeeeeeeeeee="col-sm-2">
-            @Html. .DisplayNameFor (modelo => modelo.Movie.Genre)
+            @Html .DisplayNameFor (modelo => modelo.Movie.Genre).
         </dt>
         <dd classeeeeeeeeee="col-sm-10">
-            @Html. .DisplayFor (modelo => modelo.Movie.Genre)
+            @Html .DisplayFor (modelo => modelo.Movie.Genre).
         </dd>
         <dt classeeeeeeeeee="col-sm-2">
-            @Html. .DisplayNameFor (modelo => modelo.Movie.Price)
+            @Html .DisplayNameFor (modelo => modelo.Movie.Price).
         </dt>
         <dd classeeeeeeeeee="col-sm-10">
             @Html. .DisplayFor (modelo => modelo.Movie.Price)
