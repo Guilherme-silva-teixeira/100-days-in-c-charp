37e62e20b366c911ff1f2c7eadfbb8df5cd19db0
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-02-10 21:34:07 -0300

    Create form1.cs

---
+++ project of day 10/form1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace store
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            addProductPane paneForProduct = new addProductPane();
+            paneForProduct.Show();
+            Form1 inForm = new Form1();
+            inForm.Close();
+        }
+    }
+}
