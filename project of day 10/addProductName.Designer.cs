99fc9e5d6d8db248554670cd767131eef4472f76
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-02-14 16:57:12 -0300

    Update addProductName.Designer.cs

--- project of day 10/addProductName.Designer.cs
+++ project of day 10/addProductName.Designer.cs
@@ -28,6 +28,7 @@ namespace store
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addProductPane));
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -37,6 +38,9 @@ namespace store
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -53,34 +57,35 @@ namespace store
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(310, 22);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Small Light", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(16, 100);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(279, 31);
+            this.label1.Size = new System.Drawing.Size(267, 30);
             this.label1.TabIndex = 3;
             this.label1.Text = "Insira o nome do produto:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semilight", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(16, 175);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(317, 31);
+            this.label2.Size = new System.Drawing.Size(295, 30);
             this.label2.TabIndex = 5;
             this.label2.Text = "Insira a descrição do produto:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semilight", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(16, 298);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(271, 31);
+            this.label3.Size = new System.Drawing.Size(254, 30);
             this.label3.TabIndex = 7;
             this.label3.Text = "Insira o valor do produto:";
             // 
@@ -102,25 +107,56 @@ namespace store
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Variable Display", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Segoe UI Variable Small Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(527, 107);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 24);
+            this.label4.Size = new System.Drawing.Size(38, 24);
             this.label4.TabIndex = 9;
             this.label4.Text = "Ref:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(572, 108);
+            this.textBox2.Location = new System.Drawing.Point(622, 108);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.Size = new System.Drawing.Size(153, 22);
             this.textBox2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(527, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Gerar Qr-Code:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Location = new System.Drawing.Point(531, 207);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(302, 270);
+            this.panel2.TabIndex = 12;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(654, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 25);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Saiba sobre...";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // addProductPane
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.richTextBox1);
@@ -149,5 +185,8 @@ namespace store
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
     }
 }
