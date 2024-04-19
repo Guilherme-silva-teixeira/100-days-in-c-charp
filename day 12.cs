e4dbe7324d4878b5b4b5a39efd5c96c988e38ecf
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-04-19 10:18:56 -0300

    Create day 12.cs

---
+++ day 12.cs
@@ -0,0 +1,16 @@
+namespace system
+{
+  public class program
+  {
+    public static void main(String[] args)
+    {
+      try
+      {
+
+      }catch (Exception e)
+      {
+          Console.WriteLine(e.ToString());
+      }
+    }
+  }
+}
