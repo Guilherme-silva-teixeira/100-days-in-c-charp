67eec2a7c9b11be0b0d139399bdf4a07da9cbe7e
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-04-27 16:44:32 -0300

    Create gameState.cs

---
+++ day-13/assets/gameState.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_game
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakepositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+            AddSnake();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            for (int c = 1; c < 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+
+            }
+        }
+    }
+}
